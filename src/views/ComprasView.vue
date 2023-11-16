d96f426b3878bb6503fd58ed5eb8af4a62ec45ff
jazqc <109171415+jazqc@users.noreply.github.com>
2023-11-16 14:12:08 -0300

    updates

--- src/views/ComprasView.vue
+++ src/views/ComprasView.vue
@@ -1,10 +1,48 @@
 <template>
-  COMPRAS
+  <v-list lines="one">
+  <v-list-item
+    v-for="(order, index) in orders"
+    :key= "index"
+  >
+  <v-list-subheader></v-list-subheader>
+<v-list-item-title>Fecha de compra: {{ order.createdAt }}</v-list-item-title>
+<v-list-item-subtitle>{{order.status}}</v-list-item-subtitle>
+<v-list-item-subtitle>Total ${{ order.totalCost }}</v-list-item-subtitle>
+<div v-for="(item, itemIndex) in order.items" :key="itemIndex">
+<v-list-item-subtitle>{{ item.title }}</v-list-item-subtitle>
+</div>
+<v-divider></v-divider>
+<div></div>
+
+   <!-- <div>{{ order }}</div> -->
+
+
+</v-list-item>
+  </v-list>
 </template>
 
 <script>
+import axios from 'axios';
+import { useAuthStore } from '../stores/store';
+import { ref } from 'vue';
 export default {
-
+  setup() {
+    const store = useAuthStore()
+    const orders = ref([])
+    const token = store.userData.token
+     const headers = {
+       'x-token':token
+     };
+     axios.get('https://back-ecommerce-1wni1lbxi-jazqc.vercel.app/orders', {headers})
+ .then(response => {
+   orders.value = response.data.data
+console.log(orders.value)
+ })
+ .catch(error => {
+   console.error(error);
+ })
+return {orders}
+  }
 }
 </script>
 
