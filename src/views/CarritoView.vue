b2fbc0d241298865676cc1128686ff5043162563
jazqc <109171415+jazqc@users.noreply.github.com>
2023-12-20 07:15:31 -0300

    u

--- src/views/CarritoView.vue
+++ src/views/CarritoView.vue
@@ -1,58 +1,58 @@
 <template>
-    <v-table fixed-header class="custom-table">
-        <thead>
-            <tr>
-                <th class="text-left">
-                    IMG
-                </th>
-                <th class="text-left">
-                    Nombre
-                </th>
-                <th class="text-left">
-                    Precio unitario
-                </th>
-                <th class="text-left">
-                    Cantidad
-                </th>
-                <th class="text-left">
-                    Total
-                </th>
-                <th>
+  <v-table fixed-header class="custom-table">
+    <thead>
+      <tr>
+        <th class="text-left">
+          IMG
+        </th>
+        <th class="text-left">
+          Nombre
+        </th>
+        <th class="text-left">
+          Precio unitario
+        </th>
+        <th class="text-left">
+          Cantidad
+        </th>
+        <th class="text-left">
+          Total
+        </th>
+        <th>
 
-                </th>
-                <th>
+        </th>
+        <th>
 
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="item in carrito" :key="item.id">
-                <td>
-                    <v-img :src="item.product.img"></v-img>
-                </td>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in carrito" :key="item.id">
+        <td>
+          <v-img :src="item.product.img"></v-img>
+        </td>
 
-                <td>{{ item.product.title }}</td>
-                <td>${{ item.product.price }}</td>
-                <td>{{ item.quantity }}</td>
-                <td>${{ item.quantity * item.product.price }}</td>
-                
-                <td><v-btn size="large" color="surface-variant" variant="text" icon="mdi-minus-circle"
-                        @click="decrement(item)"></v-btn></td>
-                <td><v-btn size="large" color="surface-variant" variant="text" icon="mdi-delete"
-                        @click="deleteItem(item)"></v-btn></td>
+        <td>{{ item.product.title }}</td>
+        <td>${{ item.product.price }}</td>
+        <td>{{ item.quantity }}</td>
+        <td>${{ item.quantity * item.product.price }}</td>
 
-            </tr>
+        <td><v-btn size="large" color="surface-variant" variant="text" icon="mdi-minus-circle"
+            @click="decrement(item)"></v-btn></td>
+        <td><v-btn size="large" color="surface-variant" variant="text" icon="mdi-delete"
+            @click="deleteItem(item)"></v-btn></td>
 
-        </tbody>
+      </tr>
 
-    </v-table>
-    <v-divider></v-divider>
-    <div class="text-h4 font-weight-medium text-right pa-5">Total ${{ totalCarrito }}
-    
-        <v-btn type="submit" color="primary" size="x-large" tonal>
-            <router-link to="/order">COMPRAR</router-link>
-  </v-btn>
-</div>
+    </tbody>
+
+  </v-table>
+  <v-divider></v-divider>
+  <div class="text-h4 font-weight-medium text-right pa-5">Total ${{ totalCarrito }}
+
+    <v-btn type="submit" color="primary" size="x-large" tonal>
+      <router-link to="/order">COMPRAR</router-link>
+    </v-btn>
+  </div>
 </template>
 
 <script setup>
@@ -62,6 +62,11 @@ import { ref, computed } from 'vue';
 const store = useAuthStore();
 const carrito = ref(store.carrito || []);
 
+/**
+* Calculate the total price of all items in the cart
+* @returns {number} - The total price of all items in the cart
+*/
+
 const totalCarrito = computed(() => {
   let total = 0;
   for (const item of carrito.value) {
@@ -70,7 +75,18 @@ const totalCarrito = computed(() => {
   return total;
 });
 
+/**
+* Find the index of a specific item in the cart
+* @param {Object} item - The item to find in the cart
+* @returns {number} - The index of the item in the cart, or -1 if the item is not found
+*/
+
 const getIndex = (item) => carrito.value.findIndex(({ product: { id } }) => id === item.product.id);
+
+/**
+* Remove a specific item from the cart
+* @param {Object} item - The item to remove from the cart
+*/
 
 const deleteItem = (item) => {
   const index = getIndex(item);
@@ -78,6 +94,10 @@ const deleteItem = (item) => {
   updateCarrito(carrito.value);
 };
 
+/**
+* Decrease the quantity of a specific item in the cart. If is there is only one, it will remove it
+* @param {Object} item - The item to decrease the quantity of
+*/
 const decrement = (item) => {
   const index = getIndex(item);
   if (carrito.value[index].quantity > 1) {
@@ -87,6 +107,11 @@ const decrement = (item) => {
   }
   updateCarrito(carrito.value);
 };
+
+/**
+* Set the cart in the store and local storage. 
+* @param {Array}  carrito - the updated cart
+*/
 
 function updateCarrito(carrito) {
   store.setCarrito = carrito
@@ -98,8 +123,9 @@ function updateCarrito(carrito) {
 
 <style>
 thead.header {
-    font-size: 1.5em;
+  font-size: 1.5em;
 }
+
 a {
   color: #f8f8f8;
   text-decoration: none;
